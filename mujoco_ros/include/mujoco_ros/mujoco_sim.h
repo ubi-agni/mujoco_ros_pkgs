d1989df7bb126e5b434c7a403011de83e745c2a2
David Leins <dleins@techfak.uni-bielefeld.de>
2023-03-23 16:21:57 +0100

    refactoring: Replace atomic_bool with atomic_int
    This makes sure the compiler will try the best to generate machine code
    instead of relying on an external atomic library.

--- mujoco_ros/include/mujoco_ros/mujoco_sim.h
+++ mujoco_ros/include/mujoco_ros/mujoco_sim.h
@@ -222,8 +222,8 @@ typedef struct _settings
 	std::atomic_int run               = { 0 };
 	std::atomic_int loadrequest       = { 0 };
 	std::atomic_int resetrequest      = { 0 };
-	std::atomic_bool speed_changed    = { true };
-	std::atomic_bool model_valid      = { false }; // keep track if the current file to load works or fails to load
+	std::atomic_int speed_changed     = { true };
+	std::atomic_int model_valid       = { false }; // keep track if the current file to load works or fails to load
 	// multi env
 	std::atomic_int manual_env_steps = { 0 };
 
