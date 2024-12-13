b8d7143b0151b11518cb61e24ed0ecb2c767040f
Seongjin Bien <s.bien@tum.de>
2024-12-13 08:27:49 +0100

    feat: add a robot description node

--- mujoco_ros2_base/launch/mujoco_ros2_main_launch.py
+++ mujoco_ros2_base/launch/mujoco_ros2_main_launch.py
@@ -3,9 +3,25 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+import xacro
+
 def generate_launch_description():
     xml_path = os.path.join(get_package_share_directory('mujoco_ros2_base'), 'sample_xml','pendulum.xml')
     mr2c_yaml_path = os.path.join(get_package_share_directory('mujoco_ros2_base'), 'config','mujoco_ros2_plugin_configs.yaml')
+    xacro_file = os.path.join(get_package_share_directory('mujoco_ros2_base'),
+                              'sample_xml',
+                              'pendulum.urdf')
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
     return LaunchDescription([
         Node(
             package="mujoco_ros2_base",
@@ -18,5 +34,6 @@ def generate_launch_description():
                 "MujocoXml": xml_path,
                 'MujocoPluginConfigs': mr2c_yaml_path}
             ]
-        )
+        ),
+        node_robot_state_publisher
     ])
