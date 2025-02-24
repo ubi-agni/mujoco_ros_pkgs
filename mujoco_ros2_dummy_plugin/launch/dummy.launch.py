d5fecfa190e39598ba3ca396fcd51e6834b95d25
tenfoldpaper <s.bien@tum.de>
2025-02-24 15:50:14 +0100

    feat: adjust dummy launch files with lidar sensor

--- mujoco_ros2_dummy_plugin/launch/dummy.launch.py
+++ mujoco_ros2_dummy_plugin/launch/dummy.launch.py
@@ -28,6 +28,7 @@ def concatenate_ns(ns1, ns2, absolute=False):
 
 def generate_launch_description():
     mujoco_ros_path = get_package_share_directory('mujoco_ros')
+    mujoco_ros2_control_path = get_package_share_directory('mujoco_ros2_control')
     this_pkg_path = get_package_share_directory('mujoco_ros2_dummy_plugin')
     xml_path = os.path.join(this_pkg_path, 'launch', 'pendulum.xml')
     xacro_file = os.path.join(this_pkg_path, 'launch', 'pendulum.urdf')
@@ -36,16 +37,42 @@ def generate_launch_description():
     params = {'robot_description': doc.toxml()}
     ns = ''     # this must match the namespace argument under mujoco_ros2_control in the plugin's parameter yaml file. 
                 # See the ros2_control_plugins_example_with_ns.yaml file for more details.
-
+    
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        namespace= ns,
+        parameters=[params]
+    )
+    node_joint_state_publisher = Node( # RVIZ dependency
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            namespace= ns,
+            parameters=[
+                {'source_list': [concatenate_ns(ns, 'joint_states', True)],
+                 'rate': 50}],
+    )
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            FrontendLaunchDescriptionSource(mujoco_ros_path + '/launch/launch_server.launch'),
+            # FrontendLaunchDescriptionSource(mujoco_ros_path + '/launch/launch_server.launch'),
+            FrontendLaunchDescriptionSource(mujoco_ros2_control_path + '/launch/launch_server.launch'),
             launch_arguments={
                 'use_sim_time': "true",
                 'modelfile': xml_path,
                 'verbose': "true",
                 'mujoco_plugin_config': os.path.join(this_pkg_path, 'launch', 'dummy.yaml')
             }.items()
-        )
+        ),
+        node_joint_state_publisher,
+        node_robot_state_publisher,
+        Node( # RVIZ dependency; broken right now
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_state_broadcaster', '-c', concatenate_ns(ns, 'controller_manager', True)],
+            output='screen',
+        ),
+
     ])
